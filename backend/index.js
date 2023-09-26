c7aab3b759b33f709073bee3feae7cc99dab3dcf
Abhishek280999 <darwekar155@gmail.com>
2023-09-26 14:13:12 +0530

    Home page and Menu page done

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+const Stripe = require('stripe')
 
 
 const app = express()
@@ -11,7 +12,7 @@ app.use(express.json({ limit: "10mb" }))
 const POST = process.env.POST || 8080
 
 //mongodb connection 
-console.log(process.env.MONGODB_URL)
+// console.log(process.env.MONGODB_URL)
 mongoose.set("strictQuery", false)
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => console.log("connect to DB"))
@@ -130,14 +131,63 @@ app.post("/uploadProduct", async (req, res) => {
     console.log(req.body)
     const data = await productModel(req.body)
     const datasave = await data.save()
-    res.send({message : "Upload Successfully"})
+    res.send({ message: "Upload Successfully" })
 })
 
 //
 
-app.get("/product", async(req,res)=>{
+app.get("/product", async (req, res) => {
     const data = await productModel.find({})
     res.send(JSON.stringify(data))
+})
+
+// Payment getway
+
+console.log(process.env.STRIPE_SECRET_KEY)
+
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+app.post('/create-checkout-session', async (req, res) => {
+    console.log(req.body)
+    try {
+        const params = {
+            submit_type: 'pay',
+            mode: "payment",
+            payment_method_types: ['card'],
+            billing_address_collection: "auto",
+            shipping_options: [{ shipping_rate: "shr_1NmIQeSJRHZ3gTDz3ZNKg9Sg" }],
+
+            line_items: req.body.map((item) => {
+                return {
+                    price_data: {
+                        currency: "inr",
+                        product_data: {
+                            name: item.name,
+                            // images : [item.image]
+                        },
+                        unit_amount: item.price * 100,
+                    },
+                    adjustable_quantity: {
+                        enabled: true,
+                        minimum: 1,
+                    },
+                    quantity: item.qty
+                }
+            }),
+            success_url: `${process.env.FRONTEND_URL}/success`,
+            cancel_url: `${process.env.FRONTEND_URL}/cancel`,
+
+
+
+        }
+        const session = await stripe.checkout.sessions.create(params)
+        res.status(200).json(session.id)
+    }
+    catch (err) {
+        res.status(err.statusCode || 500).json(err.message)
+    }
+
 })
 
 //server is running
